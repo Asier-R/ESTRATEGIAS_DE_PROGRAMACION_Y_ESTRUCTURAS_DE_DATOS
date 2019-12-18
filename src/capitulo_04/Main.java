e6d10d5b3627d922007f6a52b23c5adc3adc81c5
Asier <monstruosaurio@gmail.com>
2019-12-18 12:22:57 +0100

    Interfaz comparable

--- src/capitulo_04/Main.java
+++ src/capitulo_04/Main.java
@@ -1,5 +1,8 @@
 package capitulo_04;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,7 +19,59 @@ public class Main {
         newX.setGpa(22);
         System.out.println(newX.toString());
 
+        BigInteger[] marra = {new BigInteger("12"),new BigInteger("29"),new BigInteger("212"),new BigInteger("22")};
+        System.out.println("Max Number: "+findMax(marra));
+
+        String[] barra = {"Asier", "Ander", "Izaskun", "Guillermo", "Leire"};
+        System.out.println("Max String: "+findMax(barra));
+
+        Person juan = new Person("Juan", 33);
+        Person ana = new Person("Ana",44);
+        Person marcos = new Person("Marcos",54);
+
+        Person[] arrP = {juan,ana,marcos};
+        System.out.println();
+        System.out.println("Juan id: "+juan.getId());
+        System.out.println("Ana id: "+ana.getId());
+        System.out.println("Marcos id: "+marcos.getId());
+        System.out.println("Max id: "+findMax(arrP));
+        System.out.println();
 
     }
 
+    /*
+    // METODO PARA ENCONTRAR EL MAXIMO DE UN BIGINTEGER
+    public static BigInteger findMax(BigInteger [] arr){
+
+        int maxIndex = 0;
+
+        for(int i=1; i<arr.length; i++)
+            if(arr[i].compareTo(arr[maxIndex]) > 0)
+                maxIndex = i;
+        return arr[maxIndex];
+    }
+
+    // METODO PARA ENCONTRAR EL MAXIMO DE UN STRING
+    public static String findMax(String [] arr){
+
+        int maxIndex = 0;
+
+        for(int i=1; i<arr.length; i++)
+            if(arr[i].compareTo(arr[maxIndex]) > 0)
+                maxIndex = i;
+        return arr[maxIndex];
+    }
+    */
+
+    // METODO PAR ENCONTRAR EL MAXIMO DE UN OBJETO
+    // SOLO VALIDO PARA CLASES QUE IMPLEMENTAN LA INTERFAZ COMPARABLE
+    public static Comparable findMax(Comparable [] arr){
+
+        int maxIndex = 0;
+
+        for(int i=1; i<arr.length; i++)
+            if(arr[i].compareTo(arr[maxIndex]) > 0)
+                maxIndex = i;
+        return arr[maxIndex];
+    }
 }
--- src/capitulo_04/Person.java
+++ src/capitulo_04/Person.java
@@ -1,18 +1,21 @@
 package capitulo_04;
 
-public class Person {
+import java.util.Random;
+
+public class Person implements Comparable{
 
     private int age;
-
     private String name;
+    private int id = 0;
 
     public Person (String name, int age){
         this.age = age;
         this.name = name;
+        this.id = (int)(Math.round(Math.random()*1000)+1);
     }
 
     public String toString(){
-        return "Name: "+getName()+"  Age: "+getAge();
+        return "Name: "+getName()+"  Age: "+getAge()+"  id: "+getId();
     }
 
     public final void setAge(int age) {
@@ -29,5 +32,21 @@ public class Person {
 
     public final String getName() {
         return name;
+    }
+
+    public final int getId(){
+        return id;
+    }
+
+    @Override
+    public int compareTo(Object person) {
+        Person p = (Person)person;
+        if(this.id < p.getId()){
+            return -1;
+        }else if(this.getId() == p.getId()){
+            return 0;
+        }else{
+            return 1;
+        }
     }
 }
