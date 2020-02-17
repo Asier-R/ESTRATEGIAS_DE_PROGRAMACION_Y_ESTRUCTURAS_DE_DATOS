ac55c947396909cc82b9d19780952ca2ab8c3725
Asier <monstruosaurio@gmail.com>
2020-02-17 14:45:58 +0100

    Ejemplos introduccion UNED

---
+++ src/capitulo_00/vehiculos/Moto.java
@@ -0,0 +1,15 @@
+package capitulo_00.vehiculos;
+
+public class Moto extends Vehiculo {
+
+	private int cilindrada;
+	
+	public Moto(Combustible combustible, int cilindrada) {
+		super(2,combustible,1);
+		this.cilindrada = cilindrada;
+		definirAsiento(0,1,16);
+	}
+	
+	public int getCilindrada() { return this.cilindrada; }	
+	public String tipoVehiculo() { return "Moto"; }
+}
