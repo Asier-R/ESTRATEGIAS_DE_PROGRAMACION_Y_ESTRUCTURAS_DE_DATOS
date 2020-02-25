e6047642eef2507d41ecb4d9974a275a58ccecf0
Asier <monstruosaurio@gmail.com>
2020-02-25 21:06:17 +0100

    actualizacion java doc comparator

--- src/capitulo_04/Comparator.java
+++ src/capitulo_04/Comparator.java
@@ -1,6 +1,14 @@
 package capitulo_04;
 
 public interface Comparator<T> {
-    // Los argumentos son los dos elementos a comparar
-    int compare(T lhs, T rhs);
+    /**
+     * Devuelve el resultado de comparar lhs y rhs.
+     * @param lhs primer objeto
+     * @param rhs segundo objeto
+     * @return < 0 si lhs es menor que rhs,
+     * 0 si lhs es igual a rhs,
+     * > 0 si lhs es mayor que rhs.
+     * @throws ClassCastException
+     */
+    int compare(T lhs, T rhs) throws ClassCastException;
 }
